39b43b622d6896e649b59639e846f6e4be23a919
Tom Benham <benji26@hotmail.co.uk>
2016-09-27 16:43:15 +0100

    10th test done

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -62,3 +62,36 @@ def number_to_full_month_name(num)
 
   return result
 end
+
+def number_to_short_month_name(num)
+  result = case num
+  when 1
+    "Jan"
+  when 2
+    "Feb"
+  when 3
+    "Mar"
+  when 4
+    "Apr"
+  when 5
+    "May"
+  when 6
+    "Jun"
+  when 7
+    "Jul"
+  when 8
+    "Aug"
+  when 9
+    "Sep"
+  when 10
+    "Oct"
+  when 11
+    "Nov"
+  when 12
+    "Dec"
+  else
+    "That's not a month"
+  end
+
+  return result
+end
